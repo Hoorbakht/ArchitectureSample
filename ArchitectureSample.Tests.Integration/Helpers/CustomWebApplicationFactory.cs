a20745194369ffbe74df96fa91cfb33ba33c17de
Mahyar Hoorbakht <mahyar.hoorbakht@outlook.com>
2024-04-03 05:04:10 +0300

    Refine UI with various enhancements for improved user experience.

--- ArchitectureSample.Tests.Integration/Helpers/CustomWebApplicationFactory.cs
+++ ArchitectureSample.Tests.Integration/Helpers/CustomWebApplicationFactory.cs
@@ -6,16 +6,20 @@ namespace ArchitectureSample.Tests.Integration.Helpers;
 
 public class CustomWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
 {
-	protected override void ConfigureWebHost(IWebHostBuilder builder) =>
-	    builder.ConfigureServices(services =>
-	    {
-		    var descriptor = services.SingleOrDefault(
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		builder.UseEnvironment("Testing");
+		builder.ConfigureServices(services =>
+		{
+			var descriptor = services.SingleOrDefault(
 				d => d.ServiceType ==
 				     typeof(DbContextOptions<ArchitectureSampleContext>));
 
-		    if (descriptor != null)
-			    services.Remove(descriptor);
+			if (descriptor != null)
+				services.Remove(descriptor);
 
-		    services.AddDbContext<ArchitectureSampleContext>(options => options.UseInMemoryDatabase("ArchitectureSampleDatabase"));
-	    }).UseEnvironment("Testing");
+			services.AddDbContext<ArchitectureSampleContext>(options =>
+				options.UseInMemoryDatabase("ArchitectureSampleDatabase"));
+		}).UseEnvironment("Testing");
+	}
 }
